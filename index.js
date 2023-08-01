56f5f77dd6b052628e2362d9e3caaf77f642c121
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2023-08-01 21:33:51 +0300

    authentification
    authentification et ajustement

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
 import postRoutes from "./routes/posts.js";
+import userRoutes from './routes/users.js';
 
 
 const app = express();
 
-app.use('/posts',postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb" , extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb" , extended: true }));
 app.use(cors());
+
+
+app.use('/posts',postRoutes);
+app.use('/user',userRoutes);
+
 
 const CONNECTION_URL = 'mongodb+srv://ranivoaritidasandy:Gohqac-jedmab-7pitgu@cluster0.nkkbyzl.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 4000; 
